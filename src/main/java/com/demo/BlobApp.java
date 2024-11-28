a7753b7b0c7348aaed672d78e199d3ca63841de0
Anand Vetrivel <anand.vetrivel@freseniusmedicalcare.com>
2024-11-28 20:21:36 +0530

    Readingg file from blob storage

---
+++ src/main/java/com/demo/BlobApp.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import com.microsoft.azure.storage.CloudStorageAccount;
+import com.microsoft.azure.storage.StorageException;
+import com.microsoft.azure.storage.blob.CloudBlobClient;
+import com.microsoft.azure.storage.blob.CloudBlobContainer;
+import com.microsoft.azure.storage.blob.CloudBlobDirectory;
+import com.microsoft.azure.storage.blob.CloudBlockBlob;
+
+import java.net.URISyntaxException;
+import java.security.InvalidKeyException;
+import java.util.Arrays;
+
+public class BlobApp {
+}
